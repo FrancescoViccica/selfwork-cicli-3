2424e33bb57f43dd9fbc61274cc1f09a9c72a5c2
Francesco Viccica <viccicafrancesco@gmail.com>
2025-03-28 09:30:31 +0100

    esercizio corretto

--- main.js
+++ main.js
@@ -1,57 +1,61 @@
-// let scelta;
-
-// do{
-//     scelta = Number(prompt(` Inserisci il numero della bevanda:\n 1 - acqua \n 2 - coca cola \n 3 - birra `));
-
-//     switch (scelta) {
-//         case 1:
-//             console.log(`E' stata selezionata l'acqua`);
-//             break;
-//             case 2:
-//             console.log(`E' stata selezionata coca cola`);
-//             break;
-//             case 3:
-//             console.log(`E' stata selezionata la birra`);
-//             break;
-
-//         default:
-//             console.log(`La scelta inserita non è valida, inserisci il numero corretto!`);
-            
-//     }
-
-// }while (scelta < 1 || scelta > 3) {
-    
-// }
-
-let scelta ;
-
-let bevanda;
+let scelta;
 
 do{
-    scelta = prompt(` Inserisci la bevanda desiderata:\n acqua \n coca cola \n birra `);
-
+    scelta = Number(prompt(` Inserisci il numero della bevanda:\n 1 - acqua \n 2 - coca cola \n 3 - birra `));
 
     switch (scelta) {
-        case bevanda = `acqua`:
+        case 1:
             console.log(`E' stata selezionata l'acqua`);
             break;
-            
-        case bevanda = `coca cola`:
+            case 2:
             console.log(`E' stata selezionata coca cola`);
             break;
-
-        case bevanda = `birra`:
+            case 3:
             console.log(`E' stata selezionata la birra`);
             break;
 
         default:
-            console.log(`La scelta inserita non è valida, inserisci la bevanda corretta!`);
+            scelta = 0;
+            console.log(`La scelta inserita non è valida, devi inserire il numero corretto!`);
             
     }
 
- }while (scelta != bevanda) {
+      }while (scelta < 1 || scelta > 3) {
+
+        
+    }
     
- } 
+
+
+// let scelta ;
+
+// let bevanda;
+
+// do{
+//     scelta = prompt(` Inserisci la bevanda desiderata:\n acqua \n coca cola \n birra `);
+
+
+//     switch (scelta) {
+//         case bevanda = `acqua`:
+//             console.log(`E' stata selezionata l'acqua`);
+//             break;
+            
+//         case bevanda = `coca cola`:
+//             console.log(`E' stata selezionata coca cola`);
+//             break;
+
+//         case bevanda = `birra`:
+//             console.log(`E' stata selezionata la birra`);
+//             break;
+
+//         default:
+//             console.log(`La scelta inserita non è valida, inserisci la bevanda corretta!`);
+            
+//     }
+
+//  }while (scelta != bevanda) {
+    
+//  } 
  
  
 
